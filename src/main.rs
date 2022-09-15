4306b0eab2ef989fb5a604798e9cbce4d1dcbcf2
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2022-09-16 00:39:38 +0900

    Make it work

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 mod chapter_number;
 
-use std::process;
+use mdbook::errors::Error;
+use std::{io, process};
+use mdbook::preprocess::{CmdPreprocessor, Preprocessor};
 use structopt::StructOpt;
+use crate::chapter_number::ChapterNumber;
 
 #[derive(StructOpt)]
 struct Opt {
@@ -12,16 +15,26 @@ struct Opt {
 #[derive(StructOpt)]
 enum Command {
     /// Check whether a renderer is supported by this preprocessor
-    Supports { _renderer: String },
+    Supports { renderer: String },
 }
 
 fn main() {
     let opts = Opt::from_args();
 
-    if let Some(Command::Supports { _renderer: _ }) = opts.command {
+    if let Some(Command::Supports { renderer: _ }) = opts.command {
+        println!("hit!");
         process::exit(0);
-    } else {
-        eprintln!("out");
+    } else if let Err(e) = process() {
+        eprintln!("{}", e);
         process::exit(1);
     }
+}
+
+fn process() -> Result<(), Error> {
+    let (ctx, book) = CmdPreprocessor::parse_input(io::stdin())?;
+
+    let processed = ChapterNumber::new().run(&ctx, book)?;
+    serde_json::to_writer(io::stdout(), &processed)?;
+
+    Ok(())
 }
